b9f19551389811e6d2499f771d65b8b140176829
aconover42 <85565110+aconover42@users.noreply.github.com>
2021-06-21 13:50:10 -0400

    Update Recomposition-function.cpp
    added comments and clarified variables

--- Recomposition-function.cpp
+++ Recomposition-function.cpp
@@ -4,17 +4,21 @@
 using namespace std;
 
 float recombination (float E, float F, float rho)
+//Takes input of change in ionized energy (E), electric field (F), and argon density (rho)
 {
-  float r;
-  float i;
-  float j;
-  i=(0.930 + (0.212/(F * rho))*(E));
-  r= log(i);
-  j= r / (0.212 / ( F * rho));
-  return j;
+  float value;
+  float numer;
+  float total;
+  //The following calculations are based off of the modified box model used in the ICARUS experiment, with constant values taken from the brookhaven page on liquid argon TPCs
+  value=(0.930 + (0.212/(F * rho))*(E));
+  numer= log(value);
+  total= numer / (0.212 / ( F * rho));
+  //returns the amount of ionized energy that survived recombination
+  return total;
 }
 
 int main ()
+//a quick program to test our calculation
 {
   float z;
   z = recombination (1, 1, 1);
